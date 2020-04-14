2360f97872a9168f4629f903cb164edec61d29e6
strockbecca <strockbecca@gmail.com>
2020-04-14 15:37:34 +0200

    solution

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -2,8 +2,38 @@
 public class WordWrap {
 
     public static String wordWrap(String input, int columnNumber) {
+        int i = 0;
+        int count;
+        int length = input.length();
 
-        // TODO
+        System.out.println("Length of a String is: " + length);
+        //System.out.println("test" + input.charAt(4));
+
+        StringBuffer buffer = new StringBuffer(input);
+
+       for (i = 0, count = 0; i < length ; count = 0 , i++) {
+
+           if (i < length - columnNumber){
+
+               if (input.charAt(i + columnNumber ) == ' ') {
+
+                   buffer.setCharAt(i + columnNumber , '\n');
+                    input = buffer.toString();
+                    i = i + columnNumber;
+
+                } else {
+
+                    while (input.charAt(i + columnNumber - count) != ' '){
+                        count++;
+                    }
+
+                    buffer.setCharAt(i + columnNumber - count, '\n');
+                    input = buffer.toString();
+                    i = i + columnNumber - count;
+
+                }
+           }
+       }
 
         return input;
     }
