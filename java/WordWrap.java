44459e5a8090497f67c88bc4beddaa86b9285965
strockbecca <strockbecca@gmail.com>
2020-04-14 15:38:24 +0200

    clean code

--- java/WordWrap.java
+++ java/WordWrap.java
@@ -6,9 +6,6 @@ public class WordWrap {
         int count;
         int length = input.length();
 
-        System.out.println("Length of a String is: " + length);
-        //System.out.println("test" + input.charAt(4));
-
         StringBuffer buffer = new StringBuffer(input);
 
        for (i = 0, count = 0; i < length ; count = 0 , i++) {
